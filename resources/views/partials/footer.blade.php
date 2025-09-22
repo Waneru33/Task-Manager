26897a2bce0fa7c5ef792d6582cce58cd60929a0
adeyyyx <abiimay55@gmail.com>
2025-09-23 04:37:23 +0700

    feat: footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+
+<footer>
+    <!-- Bottom -->
+    <div class="border-t border-gray-700 mt-8 pt-4 text-center text-sm text-gray-500">
+        &copy; {{ date('Y') }} TaskManager. All rights reserved.
+    </div>
+</footer>
